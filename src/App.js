dbb6287c36facbfd8843337c4c12809bf848dec8
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2023-04-29 16:57:19 -0400

    setting up the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  async function fetchData() {
+    let response = await getAllPokemon(initialUrl);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
+
     </div>
   );
 }
 
 export default App;
+
